184bccff165078e465a44b6187ce0e17c25f5bab
Matt <mstewz3646@gmail.com>
2019-10-06 22:50:04 -0700

    got stretch goals.

--- js/app.js
+++ js/app.js
@@ -1,81 +1,44 @@
 'use strict';
 
-var correctAnswers = 0;
+var totalScore = 0;
 
-var username = prompt('May I have your name please?');
-console.log('this is a username inputted: ' + username);
+var username;
+var bioQuest = [];
+var responseKey = ['no', 'yes', 'yes', 'no', 'no'];
+
+bioQuest.push('First question ' + username + ', "yes" or "no": Matt was born in Redding, California.');
+bioQuest.push('Second question, and please answer with a "yes" or "no" ' + username + ': after high school Matt moved to Chico.');
+bioQuest.push('Third question, and again, please answer with a "yes" or "no", ' + username + ': after living in Chico and having to move back in with his parents, Matt moved to Santa Barbara.');
+bioQuest.push('Fourth question and thanks for playing along. Please respond with a "yes" or "no", ' + username + ': after moving to San Franciso, Matt majored in 3d Design.');
+bioQuest.push('Fifth and final question and please answer with a "yes" or "no": the overall goal for Matt is to join forces with evil and conquer the world! Muahahaha!');
 
 function allBio() {
-
+  username = prompt('May I have your name please?');
   alert('Hello ' + username + '! Welcome to Matt\'s little website! Now it is time for a quiz regarding Matt!');
 
-  var cityBorn = prompt('First question ' + username + ', "yes" or "no": Matt was born in Redding, California.');
-
-  if (cityBorn.toLowerCase() === 'no') {
-    alert('You are correct!');
-    correctAnswers++;
-  } else {
-    alert('Nope, unfortunately you are incorrect.');
-  }
-
-  var collegeCity = prompt('Second question, and please answer with a "yes" or "no" ' + username + ': after high school Matt moved to Chico.');
-
-  if (collegeCity.toLowerCase() === 'yes') {
-    alert('You got it!');
-    correctAnswers++;
-  } else {
-    alert('That is NOT the correct answer. Sorry!');
-  }
-
-  var santaBarbara = prompt('Third question, and again, please answer with a "yes" or "no", ' + username + ': after living in Chico and having to move back in with his parents, Matt moved to Santa Barbara.');
-
-  if (santaBarbara.toLowerCase() === 'yes') {
-    alert('You are a genius and gave the correct answer.');
-    correctAnswers++;
-  } else {
-    alert('I am sorry to inform you that you are incorrect.');
-  }
-
-  var graphicDesign = prompt('Fourth question and thanks for playing along. Please respond with a "yes" or "no", ' + username + ': after moving to San Franciso, Matt majored in 3d Design.');
-
-  if (graphicDesign.toLowerCase() === 'no') {
-    alert('Wow. That one was tricky, you are a superior being.');
-    correctAnswers++;
-  } else {
-    alert('I regret to inform you that you have answered this question incorrectly.');
-  }
-
-  var evil = prompt('Fifth and final question and please answer with a "yes" or "no": the overall goal for Matt is to join forces with evil and conquer the world! Muahahaha!');
-
-  if (evil.toLowerCase() === 'no' ) {
-    alert('You are correct, he just wants to be happy. :)');
-    correctAnswers++;
-  } else {
-    alert('Unfortunatly, you think Matt is an evil being who is trying to conquer the world. As much as you would want Matt to be your evil overlord, he does not want this for you. He just wants to be happy.');
-  }
+  for (var i = 0; i < bioQuest.length - 1; ++i) {
+    if(prompt(bioQuest[i]).toLowerCase() === responseKey[i]) {
+      alert('You are correct!');
+      totalScore++;
+    } else {
+      alert('Nope, unfortunately you are incorrect.');
+    }
+  };
 
   alert('Thanks again for wasting your time answering some questions about Matt and his weird little life, ' + username + ', please enjoy the rest of your life! :D');
 }
 
-function startGame() {
-  allBio();
-  numberGame();
-  guessingGame();
-}
-
-
 function numberGame() {
-
   var attempts = 0;
-  var correctAge = 37;
-
+  var correctAge = Math.floor(Math.random() * 10);
+  console.log(correctAge);
   alert('Guess Matt\'s age');
 
   while(attempts < 6) {
     var response = parseInt(prompt('Guess?'));
     if(response === correctAge) {
       alert('Congratulations, that is correct!');
-      correctAnswers++;
+      totalScore++;
       break;
     } else if (response < correctAge) {
       attempts++;
@@ -85,6 +48,7 @@ function numberGame() {
       alert('Too high, try again!');
     }
   }
+  alert('Let\s move on.');
 }
 
 function guessingGame() {
@@ -98,7 +62,7 @@ function guessingGame() {
     for (var i = 0; i < sports.length - 1; i++) {
       if(guessSport.toLowerCase() === sports[i]) {
         alert('Congrats! ' + guessSport + ' is a sport I played!');
-        correctAnswers++;
+        totalScore++;
         correctGuess = true;
       }
     }
@@ -108,15 +72,18 @@ function guessingGame() {
       attempts++;
     } else {
       totalCorrect();
-      alert('Game over.');
       attempts++;
     }
   }
 }
 
 function totalCorrect() {
-  alert('Well done, you got ' + correctAnswers + ' correct.');
+  alert('Well done, you got ' + totalScore + ' correct.');
 }
 
+function startGame() {
+  allBio();
+  numberGame();
+  guessingGame();
+}
 
-startGame();
